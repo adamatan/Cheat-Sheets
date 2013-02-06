72d70a62ca89c4c6b9d45e6922ed969b13daae79
Adam Matan <adam@matan.name>
2013-02-06 17:21:35 +0200

    Update md/cli.bash

--- md/cli.bash
+++ md/cli.bash
@@ -1,5 +1,6 @@
 # cut
-cut -d" " -f1,5      # Delimeter and miltiple fields
+cut -d" " -f1,5              # Delimeter and miltiple fields
+tr -s ' ' | cut -d' ' -f3    # Unify multiple occurances of delimiter
 
 # grep
 grep -o 'ne1.*com'   # Print matching pattern, not entire line
