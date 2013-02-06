bf605b02d68556a3a6f057ab9511fde7f239eae8
Adam Matan <adam@matan.name>
2013-02-06 15:49:07 +0200

    Update md/cli.bash

--- md/cli.bash
+++ md/cli.bash
@@ -3,3 +3,6 @@ cut -d" " -f1,5      # Delimeter and miltiple fields
 
 # grep
 grep -o 'ne1.*com'   # Print matching pattern, not entire line
+
+# ssh
+ssh -oStrictHostKeyChecking=no host   # Automatically accept foreign key
