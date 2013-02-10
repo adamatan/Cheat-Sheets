ee0922be82aa4fa5f9113e20728c593cee7849b1
Adam Matan <adam@matan.name>
2013-02-10 12:55:03 +0200

    Update md/cli.bash

--- md/cli.bash
+++ md/cli.bash
@@ -3,7 +3,10 @@ cut -d" " -f1,5              # Delimeter and miltiple fields
 tr -s ' ' | cut -d' ' -f3    # Unify multiple occurances of delimiter
 
 # grep
-grep -o 'ne1.*com'   # Print matching pattern, not entire line
+grep -o 'ne1.*com'                        # Print matching pattern, not entire line
+
+# grep boolean. 
+printf "ab\nbc\ncd\n" | grep -E "a|d"     # OR. prints ab, cd
 
 # ssh
 ssh -o StrictHostKeyChecking=no host   # Automatically accept foreign key
