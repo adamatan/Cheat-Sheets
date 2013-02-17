ad5412799c2e99b33ed58cd7877d44f1fed3d1c6
Adam Matan <adam@matan.name>
2013-02-17 15:00:20 +0200

    Update md/cli.bash

--- md/cli.bash
+++ md/cli.bash
@@ -7,8 +7,9 @@ grep -o 'ne1.*com'                        # Print matching pattern, not entire l
 printf "ab\nbc\ncd\n" | grep -E "a|d"     # Boolean OR. prints ab, cd
 
 # ssh
-ssh -o StrictHostKeyChecking=no host   # Automatically accept foreign key
-ssh -o ConnectTimeout=10        host   # Set timeout (seconds) 
+ssh -o StrictHostKeyChecking=no   # Automatically accept foreign key
+ssh -o ConnectTimeout=10          # Set timeout (seconds) 
+ssh -o LogLevel=quiet             # Supress "man in middle" warnings. Generally a bad idea
 
 # vim
 vim scp://user@server//path/to/file    # Edit remote file (Hit Enter when filename shows up")
